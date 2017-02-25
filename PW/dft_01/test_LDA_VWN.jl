eec65c38a735572224fa915038d170e600d12af0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-02-25 15:42:22 +0700

    CMU Serif and CMU Typewriter Text are not working ?

---
+++ PW/dft_01/test_LDA_VWN.jl
@@ -0,0 +1,15 @@
+include("LDA_VWN.jl")
+
+function test_main()
+
+  rhoe = 1.5*ones(5)
+
+  epsxc = excVWN( rhoe )
+  depsxc = excpVWN( rhoe )
+
+  println(epsxc)
+  println(depsxc)
+
+end
+
+test_main()
---
+++ general/test_kron.jl
@@ -0,0 +1,30 @@
+function test_main()
+
+  const ⊗ = kron
+
+  srand(1234)
+  A = rand(3,3); A = 0.5*(A+A')
+  B = rand(3,3); B = 0.5*(B+B')
+  C = rand(3,3); C = 0.5*(C+C')
+
+  term1 = A ⊗  eye(3) ⊗  eye(3)
+  term2 = eye(3) ⊗ B ⊗ eye(3)
+  term3 = eye(3) ⊗ eye(3) ⊗ C
+
+  Lapl = term1 + term2 + term3
+
+  invLapl = inv(Lapl)
+
+  invterm1 = inv(term1)
+  invterm2 = inv(term2)
+  invterm3 = inv(term3)
+
+  # these should be zeros
+  diff1 = invterm1 - inv(A) ⊗ eye(3) ⊗ eye(3)
+  # ...
+
+  # TODO: Find relationship between invterm1 and invLapl
+
+end
+
+
