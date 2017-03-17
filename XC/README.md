2500c0355ffb5f11df2319c82ed40b05df38713b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-03-17 08:06:43 +0700

    Add README for XC and comparison with the old LDA_VWN.jl

---
+++ XC/README.md
@@ -0,0 +1,5 @@
+
+The result using from `LDA_VWN.jl` is the same as using LibXC with
+`XC_LDA_X` + `XC_LDA_C_VWN`.
+
+
--- XC/call_libxc.jl
+++ XC/call_libxc.jl
@@ -3,7 +3,7 @@ const XC_LDA_C_VWN = 7  # Vosko, Wilk, & Nusair (5)
 const XC_GGA_X_PBE = 101
 const XC_GGA_C_PBE = 130
 
-function test_main()
+function test_main(id_x, id_c)
 
   ρ = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
   σ = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
@@ -14,16 +14,31 @@ function test_main()
 
   ccall( (:ffrlibxc, "ffrlibxc"), Void,
          (Int, Int, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}),
-         XC_LDA_X, N, ρ, σ, ϵ_x )
+         id_x, N, ρ, σ, ϵ_x )
 
   ccall( (:ffrlibxc, "ffrlibxc"), Void,
          (Int, Int, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}),
-          XC_GGA_C_PBE, N, ρ, σ, ϵ_c )
+          id_c, N, ρ, σ, ϵ_c )
 
+  @printf("\n")
   for i = 1:N
     @printf("%d %f %f %f\n", i, ρ[i], σ[i], ϵ_x[i] + ϵ_c[i])
   end
 
 end
 
-test_main()
+include("LDA_VWN.jl")
+function test_my_VWN()
+  ρ = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+  ϵ = excVWN(ρ)
+  N = size(ρ)[1]
+  for i = 1:N
+    @printf("%d %f %f\n", i, ρ[i], ϵ[i])
+  end
+end
+
+test_main(XC_LDA_X, XC_LDA_C_VWN)
+
+test_my_VWN()
+
+#test_main(XC_GGA_X_PBE, XC_GGA_C_PBE)
