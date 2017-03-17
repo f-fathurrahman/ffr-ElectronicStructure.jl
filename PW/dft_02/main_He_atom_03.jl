97f51d4638ee43ab47c6e0231fac9dce0f42c7ba
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-03-17 15:45:51 +0700

    Correct main_He_atom

--- PW/dft_02/main_He_atom_03.jl
+++ PW/dft_02/main_He_atom_03.jl
@@ -16,10 +16,9 @@ include("solve_poisson.jl")
 include("LDA_VWN.jl")
 include("Kprec.jl")
 
-include("structure_factor.jl")
-include("gen_rho.jl")
-include("gen_dr.jl")
-include("calc_ewald.jl")
+include("../common/gen_dr_center.jl")
+include("../common/calc_strfact_v2.jl")
+include("../common/calc_ewald_v2.jl")
 
 function test_main( ecutwfc_Ry::Float64 )
 
@@ -44,15 +43,14 @@ function test_main( ecutwfc_Ry::Float64 )
   @printf("Compression: actual, theor: %f , %f\n", actual, theor)
 
   # He atom
-  const Zatm = 2;
+  const Zatm = 2.0;
   const Nstates = 1
   const Focc = [2.0]
 
   Xpos = reshape( [0.0, 0.0, 0.0], (3,1) )
 
-  Sf = structure_factor( Xpos, G )
-
-  E_nn = calc_ewald( pw, Xpos, Sf )
+  Sf = calc_strfact( Xpos, 1, [1], pw.gvec.G )
+  E_nn = calc_ewald( pw, Sf, Xpos, 1, [1], [Zatm] )
 
   Vg = zeros(Complex128,Npoints)
   prefactor = -4*pi*Zatm/Ω
@@ -60,11 +58,9 @@ function test_main( ecutwfc_Ry::Float64 )
     Vg[ig] = prefactor/G2[ig]
   end
   V_ionic = real( G_to_R(Ns, Vg .* Sf) ) * Npoints
-
+  V_ionic = reshape( V_ionic, (Npoints) )
 
   psi, Energies, Potentials = kssolve_Emin_cg( pw, V_ionic, Focc, Nstates, NiterMax=1000 )
-
-  #
   Y = ortho_gram_schmidt(psi)
   mu = Y' * op_H( pw, Potentials, Y )
   evals, evecs = eig(mu)
