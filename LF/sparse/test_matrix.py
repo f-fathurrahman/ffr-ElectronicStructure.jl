b39d3e6718c463a837c4a9ba96e9bce95cde03ac
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-03-25 00:54:04 +0700

    Create a better script with programmatically build symbolic matrices

--- LF/sparse/test_matrix.py
+++ LF/sparse/test_matrix.py
@@ -1,39 +1,13 @@
 from sympy import *
 
-# TODO: Create variable programmatically
-Nx = 3
-# Symbols for matrix elements
-x_11, x_12, x_13 = symbols("x_11 x_12 x_13")
-x_21, x_22, x_23 = symbols("x_21 x_22 x_23")
-x_31, x_32, x_33 = symbols("x_31 x_32 x_33")
-#
-DDX = Matrix([ [x_11, x_12, x_13], [x_21, x_22, x_23], [x_31, x_32, x_33] ])
-#x_11, x_12 = symbols("x_11 x_12")
-#x_21, x_22 = symbols("x_21 x_22")
-##
-#DDX = Matrix([ [x_11, x_12], [x_21, x_22] ])
-
-Ny = 2
-#y_11, y_12, y_13 = symbols("y_11 y_12 y_13")
-#y_21, y_22, y_23 = symbols("y_21 y_22 y_23")
-#y_31, y_32, y_33 = symbols("y_31 y_32 y_33")
-##
-#DDY = Matrix([ [y_11, y_12, y_13], [y_21, y_22, y_23], [y_31, y_32, y_33] ])
-y_11, y_12 = symbols("y_11 y_12")
-y_21, y_22 = symbols("y_21 y_22")
-#
-DDY = Matrix([ [y_11, y_12], [y_21, y_22] ])
-
-Nz = 3
-z_11, z_12, z_13 = symbols("z_11 z_12 z_13")
-z_21, z_22, z_23 = symbols("z_21 z_22 z_23")
-z_31, z_32, z_33 = symbols("z_31 z_32 z_33")
-DDZ = Matrix([ [z_11, z_12, z_13], [z_21, z_22, z_23], [z_31, z_32, z_33] ])
-
-# Identity matrices
-Ix = eye(Nx)
-Iy = eye(Ny)
-Iz = eye(Nz)
+def createMatrix( symb, N ):
+    rows = []
+    for i in range(1,N+1):
+        str1 = symb + "_" + str(i) + "1:" + str(i) + str(N+1)
+        #print str1
+        rows.append( list( symbols(str1) ) )
+    #
+    return Matrix(rows)
 
 
 # A and B should be square matrices
@@ -56,37 +30,73 @@ def do_kron( A, B ):
     return Z
 
 
+STR_START = """
+\\documentclass[fleqn,a3paper,9pt]{article}
+\\usepackage[a3paper]{geometry}
+\\geometry{verbose,tmargin=2cm,bmargin=2cm,lmargin=2cm,rmargin=2cm}
+\\usepackage{amsmath}
 
-tmp = do_kron(DDX,Iy)
-nablax = do_kron(tmp,Iz)
-print("")
-print("\\newpage")
-print("\\begin{equation*}")
-print(latex(nablax))
-print("\\end{equation*}")
-print("")
+\\begin{document}
 
-tmp = do_kron(Ix,DDY)
-nablay = do_kron(tmp,Iz)
-print("")
-print("\\newpage")
-print("\\begin{equation*}")
-print(latex(nablay))
-print("\\end{equation*}")
-print("")
+\\pagestyle{empty}
 
-tmp = do_kron(Ix,Iy)
-nablaz = do_kron(tmp,DDZ)
-print("")
-print("\\newpage")
-print("\\begin{equation*}")
-print(latex(nablaz))
-print("\\end{equation*}")
-print("")
+{\\footnotesize
+"""
 
-print("")
-print("\\newpage")
-print("\\begin{equation*}")
-print(latex(nablax + nablay + nablaz))
-print("\\end{equation*}")
-print("")
+STR_END = """
+}
+\\end{document}
+"""
+
+def do_latex( Nx, Ny, Nz ):
+
+    DDX = createMatrix( "x", Nx )
+    DDY = createMatrix( "y", Ny )
+    DDZ = createMatrix( "z", Nz )
+
+    # Identity matrices
+    Ix = eye(Nx)
+    Iy = eye(Ny)
+    Iz = eye(Nz)
+
+    tmp = do_kron(DDX,Iy)
+    nablax = do_kron(tmp,Iz)
+
+    print(STR_START)
+
+    print("")
+    print("\\newpage")
+    print("\\begin{equation*}")
+    print(latex(nablax))
+    print("\\end{equation*}")
+    print("")
+
+    tmp = do_kron(Ix,DDY)
+    nablay = do_kron(tmp,Iz)
+    print("")
+    print("\\newpage")
+    print("\\begin{equation*}")
+    print(latex(nablay))
+    print("\\end{equation*}")
+    print("")
+
+    tmp = do_kron(Ix,Iy)
+    nablaz = do_kron(tmp,DDZ)
+    print("")
+    print("\\newpage")
+    print("\\begin{equation*}")
+    print(latex(nablaz))
+    print("\\end{equation*}")
+    print("")
+
+    print("")
+    print("\\newpage")
+    print("\\begin{equation*}")
+    print(latex(nablax + nablay + nablaz))
+    print("\\end{equation*}")
+    print("")
+
+    print(STR_END)
+
+
+do_latex( 3, 3, 3 )
