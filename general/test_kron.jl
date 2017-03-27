1caf0dcf2c61f845dadc8f3f0e0fa4c6701641bd
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-03-27 17:30:56 +0700

    Add test_small.jl for comparison with ffr-LFDFT

--- general/test_kron.jl
+++ general/test_kron.jl
@@ -1,13 +1,13 @@
-function test_main()
+const ⊗ = kron
 
-  const ⊗ = kron
+function test_main()
 
   srand(1234)
   A = rand(3,3); A = 0.5*(A+A')
   B = rand(3,3); B = 0.5*(B+B')
   C = rand(3,3); C = 0.5*(C+C')
 
-  term1 = A ⊗  eye(3) ⊗  eye(3)
+  term1 = A ⊗ eye(3) ⊗ eye(3)
   term2 = eye(3) ⊗ B ⊗ eye(3)
   term3 = eye(3) ⊗ eye(3) ⊗ C
 
@@ -21,10 +21,9 @@ function test_main()
 
   # these should be zeros
   diff1 = invterm1 - inv(A) ⊗ eye(3) ⊗ eye(3)
-  # ...
+  diff2 = invterm2 - eye(3) ⊗ inv(B) ⊗ eye(3)
+  diff3 = invterm3 - eye(3) ⊗ eye(3) ⊗ inv(C)
 
   # TODO: Find relationship between invterm1 and invLapl
 
 end
-
-
