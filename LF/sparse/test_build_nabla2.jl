c9aa91e86e235ca30610b838c3e0dc68952755e9
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-04-01 22:55:22 +0700

    Add sorting rows in build_nabla2.jl. It is functional now, although slightly slower compared to Kronecker product, it is however more memory-friendly

--- LF/sparse/test_build_nabla2.jl
+++ LF/sparse/test_build_nabla2.jl
@@ -2,9 +2,7 @@ push!(LOAD_PATH, "../LF_common/")
 using m_LF3d
 
 include("../LF_common/sparse_LF3d.jl")
-
-using PyPlot
-const plt = PyPlot
+include("build_nabla2.jl")
 
 function test_main( NN::Array{Int64} )
 
@@ -21,115 +19,9 @@ function test_main( NN::Array{Int64} )
   # Initialize LF
   LF = init_LF3d_c( NN, AA, BB, verbose=true )
 
-  lin2xyz = LF.lin2xyz
-  D2jl_x = LF.LFx.D2jl
-  D2jl_y = LF.LFy.D2jl
-  D2jl_z = LF.LFz.D2jl
+  @time nabla2 = build_nabla2( LF )
 
-  nnzc = Nx + Ny + Nz - 2
-  NNZ = nnzc*Npoints
-
-  rowval = zeros( Int64, NNZ )
-  nzval = zeros( Float64, NNZ )
-  colptr = zeros( Int64, Npoints + 1 )
-
-  rowGbl_x_orig = zeros( Int64, Nx )
-  rowGbl_y_orig = zeros( Int64, Ny )
-  rowGbl_z_orig = zeros( Int64, Nz )
-
-  rowGbl_x = zeros( Int64, Nx )
-  rowGbl_y = zeros( Int64, Ny )
-  rowGbl_z = zeros( Int64, Nz )
-
-  for iz = 1:Nz
-    rowGbl_z_orig[iz] = iz
-  end
-
-  rowGbl_y_orig[1] = 1
-  for iy = 2:Ny
-    rowGbl_y_orig[iy] = rowGbl_y_orig[iy-1] + Nz
-  end
-
-  rowGbl_x_orig[1] = 1
-  for ix = 2:Nx
-    rowGbl_x_orig[ix] = rowGbl_x_orig[ix-1] + Ny*Nz
-  end
-
-  # use @inbounds ?
-  colGbl = 1
-  idx_diag = 1
-
-  ip = 0
-  # rowLoc = 1:Nx, 1:Ny, 1:Nz for each gblCol
-  for colGbl = 1:Npoints
-
-    colLoc_x = ceil(Int,colGbl/(Ny*Nz))
-    #println( rowGbl_x_orig + colGbl - 1 - (colLoc_x-1)*Nz*Ny )
-
-    izz = ceil(Int,colGbl/Nz)
-    #println( rowGbl_y_orig + colGbl - 1 - (izz-1)*Nz + (colLoc_x-1)*Ny*Nz )
-
-    #println( rowGbl_z_orig + (izz-1)*Nz )
-
-    yy = colGbl - (colLoc_x-1)*Ny*Nz
-    colLoc_y = ceil( Int, yy/Nz )
-
-    colLoc_z = colGbl - (izz-1)*Nz
-
-    #println("coLLoc x, y, z:", [colLoc_x, colLoc_y, colLoc_z])
-    #println("")
-
-    # diagonal element
-    ip = ip + 1
-    rowval[ip] = colGbl
-    nzval[ip] = D2jl_x[colLoc_x,colLoc_x] + D2jl_y[colLoc_y,colLoc_y] +  D2jl_z[colLoc_z,colLoc_z]
-    #println("diagonal: ", rowval[ip])
-
-    # non
-    for ir = 1:Nx
-      if ir != colLoc_x
-        ip = ip + 1
-        rowval[ip] = rowGbl_x_orig[ir] + colGbl - 1 - (colLoc_x-1)*Nz*Ny
-        nzval[ip] = D2jl_x[ir,colLoc_x]
-        #println("x: ", rowval[ip])
-      end
-    end
-
-    for ir = 1:Ny
-      if ir != colLoc_y
-        ip = ip + 1
-        rowval[ip] = rowGbl_y_orig[ir] + colGbl - 1 - (izz-1)*Nz + (colLoc_x-1)*Ny*Nz
-        nzval[ip] = D2jl_y[ir,colLoc_y]
-        #println("y: ", rowval[ip])
-      end
-    end
-
-    for ir = 1:Nz
-      if ir != colLoc_z
-        ip = ip + 1
-        rowval[ip] = rowGbl_z_orig[ir] + (izz-1)*Nz
-        nzval[ip] = D2jl_z[ir,colLoc_z]
-        #println("z: ", rowval[ip])
-      end
-    end
-
-  end
-
-  # colptr
-  colptr[1] = 1
-  for i = 2:Npoints+1
-    colptr[i] = colptr[i-1] + nnzc
-  end
-
-  #println("ip = ", ip)
-  #println("NNZ = ", NNZ)
-
-  #println(colptr)
-  #println(rowval)
-
-  nabla2 = SparseMatrixCSC( Npoints, Npoints, colptr, rowval, nzval )
-
-  nabla2_v2 = get_Laplacian3d_kron( LF )
+  @time nabla2_v2 = get_Laplacian3d_kron( LF )
 
   #println(nabla2)
   #println(nabla2_v2)
@@ -141,6 +33,7 @@ function test_main( NN::Array{Int64} )
   srand(123)
   x = rand(Npoints)
 
+  # Compare multiplication result
   Lx = nabla2 * x
   Lx_v2 = nabla2_v2 * x
 
@@ -148,5 +41,5 @@ function test_main( NN::Array{Int64} )
 
 end
 
-
 test_main( [30, 30, 30] )
+#test_main( [3,2,4] )
--- LF/sparse/test_prec.jl
+++ LF/sparse/test_prec.jl
@@ -3,9 +3,7 @@ using m_LF3d
 include("../LF_common/sparse_LF3d.jl")
 include("../LF_common/prec_mkl_ilu0.jl")
 include("../LF_common/apply_prec_ilu0.jl")
-include("build_nabla2_x.jl")
-include("build_nabla2_y.jl")
-include("build_nabla2_z.jl")
+include("build_nabla2.jl")
 
 const ⊗ = kron
 
@@ -24,48 +22,23 @@ function test_main( NN::Array{Int64} )
   # Initialize LF
   LF = init_LF3d_c( NN, AA, BB, verbose=true )
 
-  nabla2_x = build_nabla2_x(LF)
-  nabla2_y = build_nabla2_y(LF)
-  nabla2_z = build_nabla2_z(LF)
+  # Directly build Laplacian and ILU preconditioner from MKL
+  @time nabla2 = build_nabla2(LF)
+  @time prec_nabla2 = prec_mkl_ilu0( nabla2 )
 
-  # Using Kronecker product
-  ∇2 = get_Laplacian3d_kron(LF)
+  # Using Kronecker product and ILU0 preconditioner from MKL
+  @time ∇2 = get_Laplacian3d_kron(LF)
+  @time prec_∇2 = prec_mkl_ilu0( ∇2 )
 
-  #
+  # Test the preconditioner
   srand(123)
   v = rand(Npoints)
 
-  # Test multiplication operation
-  #@time Lv_1 = nabla2_x*v + nabla2_y*v + nabla2_z*v
-  #@time Lv_2 = ∇2*v
+  Pv_1 = apply_prec_ilu0( prec_nabla2, v )
+  Pv_2 = apply_prec_ilu0( prec_∇2, v )
 
-  #@printf("Difference: %18.10e\n", sum(abs(Lv_1 - Lv_2))/Npoints)
-
-
-  # Test the preconditioner
-
-  prec_x = prec_mkl_ilu0( nabla2_x )
-  prec_y = prec_mkl_ilu0( nabla2_y )
-  prec_z = prec_mkl_ilu0( nabla2_z )
-
-  pvx = apply_prec_ilu0( prec_x, v )
-  pvy = apply_prec_ilu0( prec_y, v )
-  pvz = apply_prec_ilu0( prec_z, v )
-  Pv_1 = pvx + pvy + pvz
-  #
-  prec_∇2 = prec_mkl_ilu0( ∇2 )
-  Pv_2 = apply_prec_ilu0(prec_∇2, v)
-
-  println(sum(v))
-
-  println(sum(pvx))
-  println(sum(pvy))
-  println(sum(pvz))
-
-  println(sum(Pv_1))
-  println(sum(Pv_2))
-
+  println( sum(Pv_1 - Pv_2) )
 
 end
 
-test_main( [3, 3, 3])
+test_main( [50, 50, 50])
