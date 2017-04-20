475506b0cb9612467f69dede5f178d0ca7d228ab
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-04-20 21:47:01 +0700

    Implement coordinate transformation suggested by Gygi

---
+++ PW/common/test_Gygi.jl
@@ -0,0 +1,22 @@
+
+# r[3,Npoints]
+# atpos[3,Natoms]
+# A[Natoms]
+# R0[Natoms]
+function to_curvilinear( r::Array{Float64,2}, atpos, A, R0)
+
+  Npoints = size(r)[2]
+  Natoms = size(atpos)[2]
+
+  ξ = zeros(Float64,3,Npoints)
+
+  for ip = 1:Npoints
+    ξ[:,ip] = r[:,ip]
+    for ia = 1:Natoms
+      xx = norm( (r[:,ip] - atpos[:,ia])/R0[ia] )
+      ξ[:,ip] = ξ[:,ip] + A[ia]*( r[:,ip] - atpos[:,ia] )/R0[ia] * sech(xx)
+    end
+  end
+
+
+end
