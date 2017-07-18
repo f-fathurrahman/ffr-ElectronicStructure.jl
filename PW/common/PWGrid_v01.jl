82be8ec56806c01a5d6bc052480339414c7c7e88
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-07-18 15:03:16 +0700

    Update PWGrid_v01 to remove warnings in Julia 0.6.0, exclide xsf from git

--- PW/common/PWGrid_v01.jl
+++ PW/common/PWGrid_v01.jl
@@ -29,7 +29,7 @@ function init_grids( Ns, LatVecs, RecVecs )
   #
   Npoints = prod(Ns)
   #
-  r = Array(Float64,3,Npoints)
+  r = zeros(3,Npoints)
   ip = 0
   for k in 0:Ns[3]-1
   for j in 0:Ns[2]-1
@@ -42,8 +42,8 @@ function init_grids( Ns, LatVecs, RecVecs )
   end
   end
   #
-  G  = Array(Float64,3,Npoints)
-  G2 = Array(Float64,Npoints)
+  G  = zeros(3,Npoints)
+  G2 = zeros(Npoints)
   ip    = 0
   for k in 0:Ns[3]-1
   for j in 0:Ns[2]-1
