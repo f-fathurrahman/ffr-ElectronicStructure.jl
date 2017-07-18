bd9e691e84c00e446cb0afd034b095f8c3d2273b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-07-18 16:19:28 +0700

    Use Array{Float64} instead of zeros

--- PW/sch_03/plot_band_structure.jl
+++ PW/sch_03/plot_band_structure.jl
@@ -6,7 +6,7 @@ function plot_band_structure( evals, kpath; filename="BANDS.pdf" )
   Nkpts = size(kpath)[2]
   Nstates = size(evals)[1]
 
-  Xcoords = zeros(Float64,Nkpts)
+  Xcoords = Array{Float64}(Nkpts)
   Xcoords[1] = 0.0
   for ik = 2:Nkpts
     dk = kpath[:,ik] - kpath[:,ik-1]
