32c70a08c0707f20c0b0916e2c4abecbd99b2a81
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-12-20 12:51:48 +0700

    Minor modif to hirose_ono

--- general/hirose_ono.jl
+++ general/hirose_ono.jl
@@ -7,64 +7,68 @@ An attempt to understand Hirose-Ono paper entitled:
 """
 
 function test_main()
+    #srand(1234)
+    Nstates = 5
+    Npoints = 15
 
-  #srand(1234)
-  Nstates = 5
-  Npoints = 10
+    ϕ = rand(Npoints,Nstates) + im*rand(Npoints,Nstates)
 
-  ϕ = rand(Npoints,Nstates) + im*rand(Npoints,Nstates)
-
-  for ist = 2:Nstates
-    d = dot( ϕ[:,ist], ϕ[:,1] )
-    @printf("States: %4d d = [%18.10f,%18.10f]\n", ist, real(d), imag(d))
-  end
-
-  U = ϕ'*ϕ
-
-  Um = inv(sqrtm(U))  # Um = U^{-1/2}
-
-  ϕ_ortho = ϕ * Um
-
-  # Check orthogonalization wrt 1st states
-  println("")
-  for ist = 2:Nstates
-    d = dot( ϕ_ortho[:,ist], ϕ_ortho[:,1] )
-    @printf("States: %4d d = [%18.10f,%18.10f]\n", ist, real(d), imag(d))
-  end
-
-  #c = copy(Um)
-  c = sqrtm(U)   # <----- this is the c_{l,i}
-
-  phi = zeros(Complex128,Npoints,Nstates)
-  for i = 1:Nstates
-    for l = 1:Nstates
-      phi[:,i] = phi[:,i] + c[l,i] * ϕ_ortho[:,l]
+    println("")
+    for ist = 2:Nstates
+        d = dot( ϕ[:,ist], ϕ[:,1] )
+        @printf("States: %4d d = [%18.10f,%18.10f]\n", ist, real(d), imag(d))
     end
-  end
 
-  # Check orthogonalization wrt 1st states
-  println("")
-  for ist = 2:Nstates
-    d = dot( phi[:,ist], phi[:,1] )
-    @printf("States: %4d d = [%18.10f,%18.10f]\n", ist, real(d), imag(d))
-  end
+    U = ϕ'*ϕ
 
-  T = zeros(Complex128,Nstates,Nstates)
-  for k = 1:Nstates
-    for l = 1:Nstates
-      for i = 1:Nstates
-        T[k,l] = T[k,l] + c[k,i]*c[l,i]
-      end
+    # Calculate unitary matrix U which orthogonalizes ϕ
+    Um = inv(sqrtm(U))  # Um = U^{-1/2}
+    #println(abs.(eigvals(Um)))
+
+    ϕ_ortho = ϕ * Um
+
+    # Check orthogonalization wrt 1st state
+    println("")
+    println("Check orthogonality w.r.t 1st state")
+    for ist = 2:Nstates
+        d = dot( ϕ_ortho[:,ist], ϕ_ortho[:,1] )
+        @printf("States: %4d d = [%18.10f,%18.10f]\n", ist, real(d), imag(d))
     end
-  end
 
-  λ = eigvals(T)
-  Focc = real((2 - λ).*λ)  # the occupation number, should not exceed 1.0
+    #c = copy(Um)
+    c = sqrtm(U)   # <----- this is the c_{l,i}
 
-  println("\nOccupation numbers")
-  for ist = 1:Nstates
-    @printf("%4d %18.10f\n", ist, Focc[ist])
-  end
+    phi = zeros(Complex128,Npoints,Nstates)
+    for i = 1:Nstates
+        for l = 1:Nstates
+            phi[:,i] = phi[:,i] + c[l,i] * ϕ_ortho[:,l]
+        end
+    end
+
+    # Check orthogonalization wrt 1st states
+    println("")
+    for ist = 2:Nstates
+        d = dot( phi[:,ist], phi[:,1] )
+        @printf("States: %4d d = [%18.10f,%18.10f]\n", ist, real(d), imag(d))
+    end
+
+    T = zeros(Complex128,Nstates,Nstates)
+    for k = 1:Nstates
+        for l = 1:Nstates
+            for i = 1:Nstates
+                T[k,l] = T[k,l] + c[k,i]*c[l,i]
+            end
+        end
+    end
+
+    λ = eigvals(T)
+    Focc = real((2 - λ).*λ)  # the occupation number, should not exceed 1.0
+
+    println("\nOccupation numbers")
+    for ist = 1:Nstates
+        @printf("%4d %18.10f\n", ist, Focc[ist])
+    end
+    println("sum(Focc) = ", sum(Focc))
 
 end
 
