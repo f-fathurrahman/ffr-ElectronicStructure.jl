11c2b6be615a6c0b577b629835bfa4c81389b257
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-02-05 12:07:21 +0700

    Test fermidirac

--- PW/dft_02/fermidirac.jl
+++ PW/dft_02/fermidirac.jl
@@ -12,3 +12,14 @@ function fermidirac(ev::Float64, efermi::Float64, Tbeta::Float64)
     return 1.0/( 1.0 + exp( Tbeta*(ev - efermi)) )
 end
 
+
+function fermidirac_v2(ev::Array{Float64,1}, efermi::Float64, swidth::Float64)
+    x = (ev[:] - efermi)/(2*swidth)
+    f = (1.0 - tanh.(x)) # spin-degenerate case
+    return f
+end
+
+function fermidirac_v2(ev::Float64, efermi::Float64, swidth::Float64)
+    x = (ev - efermi)/(2*swidth)
+    return (1.0 - tanh(x))
+end
--- PW/dft_02/test_fermidirac.jl
+++ PW/dft_02/test_fermidirac.jl
@@ -1,11 +1,11 @@
 include("fermidirac.jl")
 
 function test_fermidirac()
-    Nstates = 4
+    Nstates = 5
     ev = Array{Float64}(Nstates)
-    Tbeta = 0.1
-    ev = [-2.4, -1.0, -0.5, -0.2]
-    efermi = ev[end]
+    Tbeta = 100.0  # large Tbeta, small width
+    ev = [-2.4, -1.0, -0.5, -0.2, -0.19]
+    efermi = 0.5*(ev[3] + ev[4])
     Focc = fermidirac(ev, efermi, Tbeta)
     for ist = 1:Nstates
         @printf("%f %f\n", ev[ist], Focc[ist])
@@ -13,4 +13,18 @@ function test_fermidirac()
     @printf("sum(Focc) = %f\n", sum(Focc))
 end
 
+function test_fermidirac_v2()
+    Nstates = 5
+    ev = Array{Float64}(Nstates)
+    swidth = 0.01  # Ha (same units as energy)
+    ev = [-2.4, -1.0, -0.5, -0.2, -0.19]
+    efermi = 0.5*(ev[3] + ev[4])    
+    Focc = fermidirac_v2(ev, efermi, swidth)
+    for ist = 1:Nstates
+        @printf("%f %f\n", ev[ist], Focc[ist])
+    end
+    @printf("sum(Focc) = %f\n", sum(Focc))
+end
+
 test_fermidirac()
+test_fermidirac_v2()
